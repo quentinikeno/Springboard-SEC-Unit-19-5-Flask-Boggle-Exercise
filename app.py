bedeefd33b82844052c80e49937e04eb43af7039
Quentin Ikeno <ikenoq@gmail.com>
2022-02-18 18:16:19 -1000

    Set up templates and root route

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 from boggle import Boggle
 
 boggle_game = Boggle()
+
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "oh-so-secret"
+
+debug = DebugToolbarExtension(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+ 
